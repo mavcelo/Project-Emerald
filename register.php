18091d34b49cf01bc20916b773dce9652e9c008c
SYNdiCull <syndicull@gmai.com>
2023-06-17 16:49:19 -0400

    added error checking

--- register.php
+++ register.php
@@ -13,20 +13,37 @@ session_start();
     <label for="password" type="password">Password: </label>
     <input type="password" id="password" name="password"> <br>
     <label for="email" type="text">Email: </label>
-    <input type="text" id="email" name="email">
+    <input type="text" id="email" name="email"><br>
     <input type="submit" name="submit" id="submit" value="Submit">
 </form>
 <?php
-
+$cant_save = FALSE;
 if(isset($_POST['submit'])) {
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    if (empty($_POST["username"]) || !preg_match("/[a-zA-Z0-9]+/", $username)) {
+        $nameErr = "Username is required";
+        $cant_save = TRUE;
+    }
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    if (empty($_POST["email"])) {
+        echo "Email is required";
+        $cant_save = TRUE;
+      } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $cant_save = TRUE;
+            echo "Invalid email format";
+        }
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    if (empty($_POST["password"])) {
+        echo "Password is required";
+        $cant_save = TRUE;
+    }
     $sql_u = "SELECT * FROM accounts WHERE username='$username'";
     $res_u = mysqli_query($conn, $sql_u);
 
     if(mysqli_num_rows($res_u) > 0) {
-        echo 'Taken!';
+        echo '<br>Username taken!';
+    } elseif($cant_save) {
+        echo '<br>Please ensure inputs are valid.';
     } else {
         $query = "INSERT INTO accounts (username, email, password) 
       	    	  VALUES ('$username', '$email', '$password')";
