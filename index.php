774123c5edcb555e8f5ac29f7996e800229f18d5
SYNdiCull <syndicull@gmail.com>
2023-09-03 22:21:13 -0400

    weird error fixed
    yep

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php include('db_config.php');
+
+session_start();
+$_SESSION['attempt'] = 0;
+
 ?>
 
 
@@ -57,7 +61,6 @@ if(isset($_POST['submit'])) {
   $expire_stamp = date('Y-m-d H:i:s', strtotime("+5 min"));
 
   if ($result && password_verify($_POST['password'], $result['password']) && $_SESSION['attempt'] < 5) {
-      session_start();
       session_regenerate_id();
       $id = session_id();
       unset($_SESSION['attempt']);
