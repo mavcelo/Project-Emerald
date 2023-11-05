5c4ac0ab508309b985de152d05f27f29a253a068
SYNdiCull <syndicull@gmail.com>
2023-11-05 14:43:47 -0500

    updated style page location

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,7 @@ if($_SESSION['isadmin'] == TRUE) {
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="styles/style.css">
   </head>
   <body style="overflow-x: hidden;" class="dashboard">
     <nav class="navbar bg-dark navbar-dark">
