e6e7f23360815dbfb43c4ae1d41af358d9a6ab79
SYNdiCull <syndicull@gmail.com>
2023-11-18 15:36:04 -0500

    added functionaility to remove players from db on action

---
+++ removePlayerTeam.php
@@ -0,0 +1,35 @@
+<?php
+include './db_config.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $playerName = htmlspecialchars(strip_tags($_POST['playerName']));
+    // Update the player's team_id in the players table
+    $sql = "UPDATE players SET team_id = NULL WHERE name = ?";
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt) {
+        $stmt->bind_param('s', $playerName);
+        $result = $stmt->execute();
+
+        if ($result) {
+            $response = ['success' => true];
+        } else {
+            $response = ['success' => false, 'error' => $stmt->error];
+        }
+
+        $stmt->close();
+    } else {
+        $response = ['success' => false, 'error' => $conn->error];
+    }
+
+    // Send JSON response
+    header('Content-Type: application/json');
+    echo json_encode($response);
+} else {
+    // Handle other request methods if needed
+    http_response_code(405); // Method Not Allowed
+
+}
+?>
+
